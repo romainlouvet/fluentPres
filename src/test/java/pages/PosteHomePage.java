51cadb37dfbc884750e003aa8c9525716b011bbb
romainlouvet <mobileagri@gmail.com>
2013-09-04 11:12:36 +0200

    Override isAt in Page

--- src/test/java/pages/PosteHomePage.java
+++ src/test/java/pages/PosteHomePage.java
@@ -1,36 +1,37 @@
 package pages;
 
-import org.fluentlenium.core.FluentPage;
+import static org.fest.assertions.fluentlenium.FluentLeniumAssertions.assertThat;
+import static org.fluentlenium.core.filter.FilterConstructor.withName;
 
 import java.util.List;
 
-import static org.fest.assertions.fluentlenium.FluentLeniumAssertions.assertThat;
-import static org.fluentlenium.core.filter.FilterConstructor.withName;
+import org.fluentlenium.core.FluentPage;
 
 /**
  * Created with IntelliJ IDEA. User: rlouvet Date: 02/09/13 Time: 11:30
  */
 public class PosteHomePage extends FluentPage {
-    public static final String URL_LA_POSTE = "http://www.laposte.fr/sna/rubrique.php3?id_rubrique=59";
+  public static final String URL_LA_POSTE = "http://www.laposte.fr/sna/rubrique.php3?id_rubrique=59";
 
-    @Override
-    public String getUrl() {
-        return URL_LA_POSTE;
-    }
+  @Override
+  public String getUrl() {
+    return URL_LA_POSTE;
+  }
 
-    public void isAt() {
-        assertThat(findFirst("#crit1")).isDisplayed();
-        assertThat(findFirst("#crit2")).isDisplayed();
-        assertThat(findFirst("#crit3")).isDisplayed();
-    }
+  @Override
+  public void isAt() {
+    assertThat(findFirst("#crit1")).isDisplayed();
+    assertThat(findFirst("#crit2")).isDisplayed();
+    assertThat(findFirst("#crit3")).isDisplayed();
+  }
 
-    public void searchByZipCode(String zipCode) {
-        fill("#crit1").with(zipCode);
-        submit("form", withName("cpform"));
-    }
+  public void searchByZipCode(String zipCode) {
+    fill("#crit1").with(zipCode);
+    submit("form", withName("cpform"));
+  }
 
-    public List<String> getResults() {
-        return find(".resultat").find("div").getTexts();
-    }
+  public List<String> getResults() {
+    return find(".resultat").find("div").getTexts();
+  }
 
 }
